93f7d3d4ac93f51302643851855528958c06164b
robdevop <r.jaeger@strucnamics.de>
2025-09-18 11:39:39 +0200

    Fixing hydration issues

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,15 @@
 import dynamic from 'next/dynamic';
+import { Suspense } from 'react';
 
-const AdminApp = dynamic(() => import('../components/AdminApp'), { ssr: false });
+const AdminApp = dynamic(() => import('../components/AdminApp'), {
+    ssr: false,
+    loading: () => <div>Loading Coffee Admin...</div>
+});
 
 export default function Page() {
-  return <AdminApp />;
+    return (
+        <Suspense fallback={<div>Loading Coffee Admin...</div>}>
+            <AdminApp />
+        </Suspense>
+    );
 }
